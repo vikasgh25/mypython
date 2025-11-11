60c54b41a3ac81d5a65bfdae3ea9d59c4b549b3e
vikasgh25 <vikasgh469@gmail.com>
2025-11-11 14:08:41 +0530

    Create finding largest of two number.py

---
+++ finding largest of two number.py
@@ -0,0 +1,7 @@
+# finding largest of two number
+x = int(input("enter first number: "))
+y = int(input("enter second number: "))
+if x > y:
+    print(x,"is greater")
+else:
+    print(y,"is greater")
