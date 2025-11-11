394b1408af10e021bc3b817320161170d8700d27
vikasgh25 <vikasgh469@gmail.com>
2025-11-11 14:12:23 +0530

    Create program to check whether a given character is a vowel or a consonant.py

---
+++ program to check whether a given character is a vowel or a consonant.py
@@ -0,0 +1,6 @@
+# program to check whether a given character is a vowel or a consonant.
+vowels = (input("enter character:"))
+if vowels in "aeiou or AEIOU":
+  print("vowels")
+else :
+       print("consonant")
