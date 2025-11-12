c35cf5f8822b1752d5cbf791e2f4d0ef49bebdfa
vikasgh25 <vikasgh469@gmail.com>
2025-11-12 16:12:42 +0530

    Create largernumberoftwoinfunction.py

---
+++ largernumberoftwoinfunction.py
@@ -0,0 +1,7 @@
+#largest_of_two() that accepts two numbers and prints which one is larger.
+def largest_of_two_number(a,b):
+  if a>b:
+    print("a is larger number", a)
+  else:
+      print("b is larger number",b)
+largest_of_two_number(10,20)
