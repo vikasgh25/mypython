5b19940f2105f273b489f7bbbc699015fb3e1041
vikasgh25 <vikasgh469@gmail.com>
2025-11-12 15:03:14 +0530

    Create userdefine function.py

---
+++ userdefine function.py
@@ -0,0 +1,8 @@
+#user defined function
+def hwk(a,b):
+   name = input("what is your name : ")
+   age = int(input("your age : "))
+   rollno = int(input("your roll no : "))
+
+   return a+b,a*b
+hwk(18,46)
