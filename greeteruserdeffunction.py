a411d3bd3b903dae96310e35aacb25278e74ff80
vikasgh25 <vikasgh469@gmail.com>
2025-11-12 22:46:33 +0530

    Create greeteruserdeffunction.py

---
+++ greeteruserdeffunction.py
@@ -0,0 +1,4 @@
+#greet_user() that takes a name as an argument and prints a greeting message like “Hello, Rajesh! Welcome to Python.”
+def greet(name):
+   print(f"Hello, {name}! Welcome to Python.")
+greet("Rajesh")
