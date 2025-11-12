d68aca5bcb67ce83010701135cf50abb840063c5
vikasgh25 <vikasgh469@gmail.com>
2025-11-12 16:08:26 +0530

    Create whetheroddorevennuminfunction.py

---
+++ whetheroddorevennuminfunction.py
@@ -0,0 +1,7 @@
+#checks whether a number is even or odd and prints the result.
+def check_even_odd(a):
+  if a%2== 0:
+    print("even number")
+  else:
+    print( "odd number" )
+check_even_odd(10)
